9727ae2c279fd3e47f7e508828134f68bc63227b
Mubashir Hasan <mubashirhasan716@gmail.com>
2020-10-09 21:03:53 +0530

    modified somewhat

--- src/Video.js
+++ src/Video.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import "./Video.css";
+import vid from "./sj.mp4";
 
 function Video() {
+    const[playing, setPlaying]= useState(false)
+    const videoRef= useRef(null);
+
+    const handleVideoPress= ()=>{
+
+        if(playing){
+            videoRef.current.pause();
+            setPlaying(false)
+
+        }
+        else {
+            videoRef.current.play();
+            setPlaying(true)
+        }
+        //if video is playing
+        //stop.. it
+
+        //otherwise play it
+    }
+
   return (
     <div className="video">
       <video className="video__player">
-        <source src="./sj.mp4" type="video/mp4"/>
+        <source
+            onClick={handleVideoPress}
+            src={vid}
+            type="video/mp4"
+            ref={videoRef}
+        />
 
       </video>
 
