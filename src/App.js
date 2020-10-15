6e6dd63800672c4be3ab56fce5360424194c3a68
Mubashir Hasan <mubashirhasan716@gmail.com>
2020-10-15 20:46:39 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from "react";
 import "./App.css";
 import Video from "./Video";
-import Vid from"./sj.mp4";
+import Vid from "./sj.mp4";
+import Apple from "./apple-marketing.mp4";
 
 function App() {
   return (
     <div className="app">
       <div className="app__videos">
         <Video
-            url={Vid}
+            url={Apple}
             channel={`Mub45191`}
+            song={`The React Song`}
+            description={'My TikTok'}
+            likes={1233}
+            messages={ 4563 }
+            shares={7893}
+        />
+        <Video
+          url={Vid}
+          channel={`Mub45191`}
             song={`The React Song`}
             description={'My TikTok'}
             likes={123}
             messages={ 456 }
             shares={789}
-        />
-        <Video
-            url={Vid}
         />
       </div>
     </div>
